2a3c6d5ac673217f12b089354041b34d15415abf
singhayush20 <ayushsingh20april@gmail.com>
2026-02-06 15:09:13 +0530

    removed redundant files

--- src/main/java/com/ayushsingh/doc_helper/features/chat/execution/DocChatCommand.java
+++
@@ -1,6 +0,0 @@
-package com.ayushsingh.doc_helper.features.chat.execution;
-
-import com.ayushsingh.doc_helper.features.chat.dto.ChatRequest;
-
-public record DocChatCommand(ChatRequest request, boolean webSearch) {
-}
