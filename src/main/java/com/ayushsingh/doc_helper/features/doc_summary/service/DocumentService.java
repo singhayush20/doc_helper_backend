bec1207604b3b7493993c75dcc70e6d1c3a8252e
singhayush20 <ayushsingh20april@gmail.com>
2026-02-07 11:14:27 +0530

    refactoring

--- src/main/java/com/ayushsingh/doc_helper/features/doc_summary/service/DocumentService.java
+++ src/main/java/com/ayushsingh/doc_helper/features/doc_summary/service/DocumentService.java
@@ -4,6 +4,8 @@ import com.ayushsingh.doc_helper.features.doc_summary.entity.Document;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface DocumentService {
+    
     Document createFromUpload(Long userId, MultipartFile file);
+
     Document getByIdForUser(Long documentId, Long userId);
 }
