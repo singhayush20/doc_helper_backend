e5e8bc08c478722ce5b2d4723826bf04abb943a3
singhayush20 <ayushsingh20april@gmail.com>
2026-02-08 16:56:02 +0530

    removed unused imports

--- src/main/java/com/ayushsingh/doc_helper/features/user_plan/entity/BillingProduct.java
+++ src/main/java/com/ayushsingh/doc_helper/features/user_plan/entity/BillingProduct.java
@@ -15,8 +15,6 @@ import jakarta.persistence.CollectionTable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
